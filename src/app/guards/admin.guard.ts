7759ced694036bcc0b15428de382037afeea4760
jkatindi <43222970+jkatindi@users.noreply.github.com>
2024-05-21 04:46:16 +0200

    Update admin.guard.ts

--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -4,6 +4,7 @@ import {inject} from "@angular/core";
 
 export const adminGuard: CanActivateFn = (route, state) => {
   let authService=inject(AuthenticationService)
+  
   if(!authService.isAuthenticated){
     return  false;
   }
